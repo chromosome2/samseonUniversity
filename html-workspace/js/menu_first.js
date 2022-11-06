e20fe3572c1f3dae91e7ba9a4d34d6d8379c1cea
chromosome2 <tnwls356@gmail.com>
2022-11-07 00:51:56 +0900

    sj - html수정

---
+++ html-workspace/js/menu_first.js
@@ -0,0 +1,4 @@
+$(function () {
+	$('.point').removeClass('point');
+	$('.first_menu_title').addClass('point');
+});
---
+++ html-workspace/js/menu_third.js
@@ -0,0 +1,5 @@
+$(function () {
+	$('.point').removeClass('point');
+	$('.third_menu_title').addClass('point');
+	$('.my_page_menu').css('display','inline-block');
+});
