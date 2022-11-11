6d1066cd5f7a3b086cd011a2fd65479eab9bbdc4
chromosome2 <tnwls356@gmail.com>
2022-11-11 16:22:54 +0900

    sj - admin매핑이름 수정

--- web-project/samseon/src/main/java/samseon/admin/AdminController.java
+++ web-project/samseon/src/main/java/samseon/admin/AdminController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/admin/*")
+@WebServlet("/manage/*")
 public class AdminController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
